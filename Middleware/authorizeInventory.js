ee851a71c07018b6f7eb2c66e503e0a2710246ad
gkztech <maccarthy28@gmail.com>
2026-02-14 11:21:21 -0800

    Car Detail

--- Middleware/authorizeInventory.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  if (res.locals.user && (res.locals.user.account_type === 'Employee' || res.locals.user.account_type === 'Admin')) {
-    return next();
-  }
-  // Use flash message or query parameter; here we use flash (requires connect-flash)
-  req.flash('error', 'You must be an employee or admin to access that page.');
-  res.redirect('/account/login');
-};
